6e9eb15d8aa65519ec7da8d36d258e1fdd0e2fc4
Giri Bhargavi Goud <157745222+Bhargavigoud12@users.noreply.github.com>
2025-06-11 19:12:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -106,6 +106,7 @@ def upload_file():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    serve(app, host='0.0.0.0', port=port)
+
 
 
